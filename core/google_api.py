3d9eae16c9cb4e6505f0fddd3456c63c60e51e97
siying030-maker <siying030@gmail.com>
2026-05-26 08:48:26 +0800

    fix google api import

--- core/google_api.py
+++ core/google_api.py
@@ -1,18 +1,25 @@
-import gspread
 import time
-from google.oauth2.service_account import Credentials
+import gspread
 import streamlit as st
-from core.config import SCOPES, CACHE_TTL
+from google.oauth2.service_account import Credentials
+
+CACHE_TTL = 300
+
+SCOPES = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
+]
 
 _last_call = 0
 
 def rate_limit():
     global _last_call
-    import time
 
     now = time.time()
+
     if now - _last_call < 0.3:
         time.sleep(0.3)
+
     _last_call = time.time()
 
 
@@ -22,6 +29,7 @@ def get_client():
         st.secrets["google"],
         scopes=SCOPES
     )
+
     return gspread.authorize(creds)
 
 
@@ -33,6 +41,7 @@ def open_sheet(url):
         try:
             rate_limit()
             return client.open_by_url(url)
+
         except Exception as e:
             if "429" in str(e):
                 time.sleep((i + 1) * 5)
